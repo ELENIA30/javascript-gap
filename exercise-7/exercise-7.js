51e8e65d7a8f3ca7ce39ef5bf8321e2a8a09fc35
elenia centamore <elenia.centamore@musement.com>
2022-10-03 12:38:46 +0200

    exercise-7 done

--- exercise-7/exercise-7.js
+++ exercise-7/exercise-7.js
@@ -1,13 +1,12 @@
-const cubesSum = (num) => {
-    let sum = 0;
+const compareObj = (obj1, obj2) => {
+    const obj1Props = Object.getOwnPropertyNames(obj1);
+    const obj2Props = Object.getOwnPropertyNames(obj2);
 
-    for(let i = 1; i <= num; i++){
-        sum = sum + (i*i);
+    if(obj1Props.length >= obj2Props.length) {
+        return obj1Props.every((v, i) => v === obj2Props[i]);
     }
-    return sum;
+    return obj2Props.every((v, i) => v === obj1Props[i]);
 }
+console.log(compareObj({a: 2, b: 6, e: 5}, {a: 3, b: 9, e: 4, h: 4}))
 
-console.log(cubesSum(30));
-console.log(cubesSum(4));
-
-export {cubesSum}
+export {compareObj}
--- exercise-7/exercise-7.test.js
+++ exercise-7/exercise-7.test.js
@@ -1,5 +1,14 @@
-import { cubesSum } from "./exercise-7";
+import { compareObj } from "./exercise-7";
 
-test("test cubesSum function", () => {
-    expect(cubesSum(7)).toBe(140);
+test("test compareObj function - in different case", () => {
+    expect(compareObj({a: 3, c: 7}, {a: 0, k: 8})).toBe(false)
+})
+test("test compareObj function - in equal case", () => {
+    expect(compareObj({a: 3, c: 7}, {a: 0, c: 9})).toBe(true)
+})
+test("test compareObj function - first arg length is major", () => {
+    expect(compareObj({a: 3, c: 7, v: 6}, {a: 0, c: 9})).toBe(false)
+})
+test("test compareObj function - second arg length is major", () => {
+    expect(compareObj({a: 3, c: 7}, {a: 0, c: 9, o: 6})).toBe(false)
 })
